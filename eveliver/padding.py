031c3c448e75830799d718326be1f8c65901552c
dujiaju <i@dujiaju.me>
2020-01-02 08:19:49 +0000

    1.2.0

--- eveliver/padding.py
+++ eveliver/padding.py
@@ -1,15 +1,15 @@
 import torch
 
 
-def sents2t(sentences, seq_len):
-    ret = torch.zeros(len(sentences), seq_len, dtype=torch.int64)
+def sents2t(sentences, seq_len, default=0):
+    ret = torch.ones(len(sentences), seq_len, dtype=torch.int64) * default
     for _id, sentence in enumerate(sentences):
         ret[_id, 0:len(sentence)] = torch.tensor(sentence, dtype=torch.int64)
     return ret
 
 
-def b_sents2t(batch, seq_len):
-    ret = torch.zeros(len(batch), len(batch[0]), seq_len, dtype=torch.int64)
+def b_sents2t(batch, seq_len, default=0):
+    ret = torch.ones(len(batch), len(batch[0]), seq_len, dtype=torch.int64) * default
     for bid, sentences in enumerate(batch):
         for sid, sentence in enumerate(sentences):
             ret[bid, sid, 0:len(sentence)] = torch.tensor(sentence, dtype=torch.int64)
