a1534cb6c96a47cedf7629656b400313dd1a2135
Jiaju Du <i@dujiaju.me>
2020-12-10 10:34:17 +0800

    1.16.0

--- eveliver/span.py
+++ eveliver/span.py
@@ -1,8 +1,9 @@
 import collections
 import re
-from typing import List
 import string
+from typing import List
 
+import nltk
 import torch
 
 
@@ -94,3 +95,77 @@ def findall(a: str, b: str) -> List[int]:
         else:
             break
     return matches
+
+
+def mixed_segmentation(in_str, rm_punc=False):
+    in_str = in_str.lower().strip()
+    segs_out = []
+    temp_str = ""
+    sp_char = ['-', ':', '_', '*', '^', '/', '\\', '~', '`', '+', '=',
+            '，', '。', '：', '？', '！', '“', '”', '；', '’', '《', '》', '……', '·', '、',
+            '「', '」', '（', '）', '－', '～', '『', '』']
+    for char in in_str:
+        if rm_punc and char in sp_char:
+            continue
+        if re.search(r'[\u4e00-\u9fa5]', char) or char in sp_char:
+            if temp_str != "":
+                ss = nltk.word_tokenize(temp_str)
+                segs_out.extend(ss)
+                temp_str = ""
+            segs_out.append(char)
+        else:
+            temp_str += char
+    if temp_str != "":
+        ss = nltk.word_tokenize(temp_str)
+        segs_out.extend(ss)
+    return segs_out
+
+
+def remove_punctuation(in_str):
+    in_str = in_str.lower().strip()
+    sp_char = ['-', ':', '_', '*', '^', '/', '\\', '~', '`', '+', '=',
+            '，', '。', '：', '？', '！', '“', '”', '；', '’', '《', '》', '……', '·', '、',
+            '「', '」', '（', '）', '－', '～', '『', '』']
+    out_segs = []
+    for char in in_str:
+        if char in sp_char:
+            continue
+        else:
+            out_segs.append(char)
+    return ''.join(out_segs)
+
+
+def find_lcs(s1, s2):
+    m = [[0 for i in range(len(s2)+1)] for j in range(len(s1)+1)]
+    mmax = 0
+    p = 0
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                m[i+1][j+1] = m[i][j]+1
+                if m[i+1][j+1] > mmax:
+                    mmax = m[i+1][j+1]
+                    p = i+1
+    return s1[p-mmax:p], mmax
+
+
+def cqa_f1(answer, prediction):
+    if answer == '' and prediction == '':
+        return 1
+    ans_segs = mixed_segmentation(answer, rm_punc=True)
+    prediction_segs = mixed_segmentation(prediction, rm_punc=True)
+    lcs, lcs_len = find_lcs(ans_segs, prediction_segs)
+    if lcs_len == 0:
+        return 0
+    precision = 1.0*lcs_len/len(prediction_segs)
+    recall = 1.0*lcs_len/len(ans_segs)
+    f1 = (2*precision*recall)/(precision+recall)
+    return f1
+
+
+def cqa_em(answer, prediction):
+    answer = remove_punctuation(answer)
+    prediction_ = remove_punctuation(prediction)
+    if answer == prediction_:
+        return 1
+    return 0
